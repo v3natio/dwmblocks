3b79b401763634489f2059f6f01251b92d5bfbf2
v3natio <72844565+v3natio@users.noreply.github.com>
2025-06-15 18:58:05 -0500

    bumped to dwmblocks-async

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
@@ -18,15 +18,11 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
+  X("", "sb-cpu", 10, 18) \
+  X("", "sb-wifi", 5, 4) \
+	X("", "sb-volume", 10, 10) \
+	X("", "sb-battery", 5, 3) \
+	X("", "sb-time", 60, 1) \
+	X("", "sb-date", 60, 1)
 
 #endif  // CONFIG_H
